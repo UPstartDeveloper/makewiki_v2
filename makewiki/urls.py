df045cd6c310c63c337937d48243db7c01a3663b
= <zainr7989@gmail.com>
2019-11-17 15:25:25 -0800

    [ADD] URL pattern and template for the password reset done view.

--- makewiki/urls.py
+++ makewiki/urls.py
@@ -17,12 +17,6 @@ from django.contrib import admin
 import django.contrib.auth.views as auth_views
 from django.urls import path, include
 
-"""
-CHALLENGES:
-    1. Uncomment the path() for the wiki app below. Use it to direct any request (except `/admin` URLs)
-        to the the `wiki` app's URL configuration. Use the above docstring to guide you if you feel stuck.
-    2. Make sure Django doesn't give you any warnings or errors when you execute `python manage.py runserver`.
-"""
 urlpatterns = [
     # Admin Site
     path('admin/', admin.site.urls),
@@ -33,5 +27,7 @@ urlpatterns = [
 
     # Accounts App
     path('registration/', include('accounts.urls')),
-    path('reset-password/', auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"), name="password_reset")
+    # Inspiration for these URL patterns goes to Corey Schafer's video tutorial: https://youtu.be/-tyBEsHSv7w
+    path('reset-password/', auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"), name="password_reset"),
+    path('reset-password/done', auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_done.html"), name="password_reset_done")
 ]
