4f096de6531c0cbf31e73dccdc22ca1d0ec2f92d
= <zainr7989@gmail.com>
2019-11-17 18:25:59 -0800

    [FIX] Disallowed Redirect.

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,8 +8,8 @@ from django.urls import reverse_lazy
 urlpatterns = [
     path('signup', SignUpView.as_view(), name='signup-form'),
     # Inspiration for these URL patterns goes to Corey Schafer's video tutorial: https://youtu.be/-tyBEsHSv7w
-    path('reset-password', auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html", success_url='accounts:password_reset_done'), name="password_reset"),
+    path('reset-password', auth_views.PasswordResetView.as_view(template_name="accounts/password_reset.html"), name="password_reset"),
     path('reset-password/done', auth_views.PasswordResetDoneView.as_view(template_name="accounts/password_reset_done.html"), name="password_reset_done"),
-    # path('reset-password-confirm/<uidb64/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="acccounts/password_reset_confirm.html"), name="password_reset_confirm"),
+    path('reset-password-confirm/<uidb64/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="acccounts/password_reset_confirm.html"), name="password_reset_confirm"),
 
 ]
