5c76d46f9e5e5ae4726c59a68f43165384c953a1
= <zainr7989@gmail.com>
2019-11-19 18:24:50 -0800

    [UPDATE] Views and url patterns for the new namespace.

--- wiki/urls.py
+++ wiki/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from wiki.views import PageListView, PageDetailView, PageCreate
+from wiki.views import PageListView, PageDetailView, PageCreate, PageUpdate
 
-
+app_name = 'wiki'
 urlpatterns = [
         path('', PageListView.as_view(), name='wiki-list-page'),
         path('<str:slug>', PageDetailView.as_view(), name='wiki-details-page'),
-        path('create/', PageCreate.as_view(), name="create_page_form")
+        path('create/', PageCreate.as_view(), name="create_page_form"),
+        path('update/<str:slug>/', PageUpdate.as_view(), name="update_page_form"),
 ]
--- wiki/views.py
+++ wiki/views.py
@@ -1,22 +1,25 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import FormView, CreateView, ModelFormMixin
+from django.views.generic.edit import (FormView, CreateView, ModelFormMixin,
+                                       UpdateView)
 from wiki.models import Page
 from wiki.forms import PageForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
 from django.contrib.auth.models import User
+from django.template import loader
 
 
 class PageListView(ListView):
     """ Renders a list of all Pages. """
     model = Page
+    template_name = 'wiki/list.html'
 
     def get(self, request):
         """ GET a list of Pages. """
         pages = self.get_queryset().all()
-        return render(request, 'list.html', {
+        return render(request, self.template_name, {
           'pages': pages
         })
 
@@ -24,21 +27,22 @@ class PageListView(ListView):
 class PageDetailView(DetailView):
     """ Renders a specific page based on it's slug."""
     model = Page
+    template_name = 'wiki/page.html'
 
     def get(self, request, slug):
         """ Returns a specific wiki page by slug. """
         page = self.get_queryset().get(slug__iexact=slug)
-        return render(request, 'page.html', {
+        return render(request, self.template_name, {
           'page': page
         })
 
 
 class PageCreate(CreateView):
-    """Render a form to create a new page."""
+    '''Render a form to create a new page.'''
     model = Page
     fields = ["title", "content"]
-    template_name = "add_page.html"
     object = None  # new Page to be created
+    template_name = 'wiki/add_page.html'
 
     def form_valid(self, form, request):
         """Creates the new Page, and makes the user who submitted
@@ -58,4 +62,11 @@ class PageCreate(CreateView):
         if form.is_valid():
             return self.form_valid(form, request)
         else:
-            return self.form_invalid(form, request)
+            return self.form_invalid(form)
+
+
+class PageUpdate(UpdateView):
+    '''Render a form to update a page.'''
+    model = Page
+    fields = ['title', 'content']
+    template_name_suffix = "_edit_form"
