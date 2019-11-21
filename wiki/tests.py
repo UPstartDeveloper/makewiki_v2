d52a6441b5f8956305763bae977269cf691e6348
= <zainr7989@gmail.com>
2019-11-21 15:58:26 -0800

    [ADD] Canary test case.

--- wiki/tests.py
+++ wiki/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+class WikiTestCase(TestCase):
+    def test_true_is_true(self):
+        '''Canary test to ensure the tests are able to run. Returns True.'''
+        self.assertEqual(True, True)
