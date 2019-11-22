504230e2be5d17b3560dc8e1be933209c79edfa6
= <zainr7989@gmail.com>
2019-11-21 16:12:24 -0800

    [ADD] Test for the PageList view.

--- wiki/tests.py
+++ wiki/tests.py
@@ -24,3 +24,30 @@ class WikiTestCase(TestCase):
         # Make sure the slug that was generated in Page.save()
         # matches what we think it should be.
         self.assertEqual(page.slug, "my-test-page")
+
+
+class PageListViewTests(TestCase):
+    def test_multiple_pages(self):
+        # Make some test data to be displayed on the page.
+        user = User.objects.create()
+
+        Page.objects.create(title="My Test Page", content="test", author=user)
+        Page.objects.create(title="Another Test Page", content="test", author=user)
+
+        # Issue a GET request to the MakeWiki homepage.
+        # When we make a request, we get a response back.
+        response = self.client.get('/')
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the number of pages passed to the template
+        # matches the number of pages we have in the database.
+        responses = response.context['pages']
+        self.assertEqual(len(responses), 2)
+
+        self.assertQuerysetEqual(
+            responses,
+            ['<Page: My Test Page>', '<Page: Another Test Page>'],
+            ordered=False
+        )
