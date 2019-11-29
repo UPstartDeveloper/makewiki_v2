25adef6a068233abe8a4ca1776c138b16f8a93de
= <zainr7989@gmail.com>
2019-11-29 15:37:00 -0800

    [ADD] Test for API post requests.

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from rest_framework.test import (
+    APIRequestFactory, APITestCase
+)
+from rest_framework import status
+from django.contrib.auth.models import User
+from django.urls import reverse
+from wiki.models import Page
+from api.serializers import PageSerializer
+from api.views import PageList, PageOperationsView
 
-# Create your tests here.
+
+class PageListTests(APITestCase):
+    def setUp(self):
+        '''Instantiate objects needed to test create and list view.'''
+        self.factory = APIRequestFactory()
+        self.user = User.objects.create(username='Abdullah',
+                                        email='abd@gmail.com',
+                                        password="Abdullah's passwd")
+        self.view = PageList.as_view()
+
+    def test_create_page(self):
+        '''A user is able to create a new Page if they are signed in.'''
+        # make an authenticated request to the view
+        create_url = reverse('api:wiki_list_or_create')
+        data = {'title': "New Page",
+                'author': self.user.id,
+                'content': 'This is a test page.'}
+        post_request = self.factory.post(create_url, data)
+        # test that the Page instaniates
+        response = self.view(post_request)
+        # response = self.client.post(create_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 import api.views as api_views
 
+app_name = 'api'
 urlpatterns = [
     path('pages/', api_views.PageList.as_view(), name='wiki_list_or_create'),
     path('pages/<int:pk>/', api_views.PageOperationsView.as_view(),
